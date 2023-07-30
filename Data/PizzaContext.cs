4d82638c5dd4b4a455590fefca9e570791136aba
joshturner <joshturner@JOSHTURNER268D>
2023-07-30 16:35:16 +1000

    Add project files.

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkDemo.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=pizza_app.db")
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkDemo.Data;
+using EntityFrameworkDemo.Models;
+
+using (PizzaContext context = new())
+{
+    Product meatLovers = new()
+    {
+        Name = "Meat Lovers",
+        Price = 12.95M
+    };
+    context.Products.Add(meatLovers);
+
+    Product supreme = new()
+    {
+        Name = "Supreme",
+        Price = 12.95M
+    };
+    context.Products.Add(supreme);
+
+    context.SaveChanges();
+}
